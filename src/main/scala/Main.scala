a0b38e4e9ffa5bbe288bc33de5af28d6bc54f2c7
Christoph Lange <math.semantic.web@gmail.com>
2022-01-01 00:03:42 +0100

    first specific functionality

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,26 @@
 import com.github.tototoshi.csv._
 
-@main def hello: Unit = 
+// https://docs.scala-lang.org/scala3/book/types-structural.html
+class Record(elems: (String, Any)*) extends Selectable:
+  private val fields = elems.toMap
+  def selectDynamic(name: String): Any = fields(name)
+
+type State = Record {
+  val currency: String
+  val lastPrice: BigDecimal
+}
+
+type Transaction = Record {
+  val currency: String
+  val price: BigDecimal
+}
+
+def processTx(st: State, tx: Transaction): State =
+  st
+
+@main def main: Unit = 
   val reader = CSVReader.open("trades.csv")
-  println("Hello world!")
+  val it = reader.iteratorWithHeaders
+  val tx = it.next
+  printf("%s: %s\n", tx("pair"), tx("price"))
   reader.close()
