96190471e55d0403a0ee37e2cdc5950a96b8406a
Christoph Lange <math.semantic.web@gmail.com>
2022-01-01 13:20:37 +0100

    mapping todata structure

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,5 @@
 import com.github.tototoshi.csv._
+import java.time.LocalDateTime
 
 // https://docs.scala-lang.org/scala3/book/types-structural.html
 class Record(elems: (String, Any)*) extends Selectable:
@@ -10,17 +11,45 @@ type State = Record {
   val lastPrice: BigDecimal
 }
 
+object TransactionType extends Enumeration:
+  val buy, sell = Value
+
 type Transaction = Record {
+  /** part of "pair" */
   val currency: String
-  val price: BigDecimal
+  /** "time" */
+  val time: String // TODO LocalDateTime
+  /** "type" */
+  val typ: String // TODO TransactionType.Value
+  /** "price" */
+  val price: String // TODO BigDecimal
+  /** "cost" */
+  val cost: String // TODO BigDecimal
+  /** "fee" */
+  val fee: String // TODO BigDecimal
+  /** "vol" */
+  val vol: String // TODO BigDecimal
 }
 
 def processTx(st: State, tx: Transaction): State =
   st
 
 @main def main: Unit = 
+  // TODO initialize zero State
   val reader = CSVReader.open("trades.csv")
-  val it = reader.iteratorWithHeaders
+  // Map(Foo -> a, Bar -> b, Baz -> c)
+  val it = reader.iteratorWithHeaders.map(
+    tx =>
+      Record( // TODO parse dates and numbers
+        "currency" -> tx("pair"),
+        "time" -> tx("time"),
+        "typ" -> tx("type"),
+        "price" -> tx("price"),
+        "cost" -> tx("cost"),
+        "fee" -> tx("fee"),
+        "vol" -> tx("vol")
+    ).asInstanceOf[Transaction])
+  // TODO foldLeft processTx over the collection
   val tx = it.next
-  printf("%s: %s\n", tx("pair"), tx("price"))
+  printf("%s: %s\n", tx.currency, tx.price)
   reader.close()
